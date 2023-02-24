22df62fef44ed4821f414402ccf7e76903444b20
Anik Sen <aniksen.cuet09@gmail.com>
2023-02-24 13:44:51 +0600

    deploy initial

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    margin: 10,
+    nav: true,
+    navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
